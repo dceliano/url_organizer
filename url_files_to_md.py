c475533fb7ddca3b87c05cf9b3d94c47df3d7bb5
Dom <d.celiano@yahoo.com>
2017-12-05 14:26:41 +0000

    Updated video link to HTML converter for new website format.

--- url_files_to_md.py
+++ url_files_to_md.py
@@ -1,9 +1,10 @@
 """ This script will create a markdown (.md) file based on the URL files in a certain directory.
-It will organize the links into categories based on the file structure of the directory you pass it.  """
+It will organize the links into categories based on the file structure of the directory you pass it.
+I typically use this script to convert video links stored in a directory to URLs, but in theory it can be used
+for any similar task. """
 import os
 
 root_dir = input("Input the root directory to convert to markdown: ")
-#root_dir = "video_links"
 output_filename = input("What do you want the output file to be called? ")
 open(output_filename, 'w')
 output_file = open(output_filename, 'w')
@@ -14,20 +15,24 @@ for root, dirs, files in os.walk(root_dir): #for each directory and subdirectory
 	path = root.split('/')
 	level = len(path) - 1
 	current_dir = os.path.basename(root)
-	index += "\n<div>\n"
-	index += ("&nbsp&nbsp&nbsp&nbsp&nbsp" * level) + "<A href=#" + current_dir + ">- " + current_dir + "</A>\n" #Table of Contents -- each bullet needs to be indented by 4 spaces for a nested list in markdown
-	index += "\n</div>\n"
+	#index += '\n<div>\n'
+	#Table of Contents -- each bullet needs to be indented by 4 spaces for a nested list in markdown
+	if(level != 0):
+		index += ("&nbsp&nbsp&nbsp&nbsp&nbsp" * (level - 1)) + "<a href=#" + current_dir + ">- " + current_dir + "</a><br>\n"
+	#index += "\n</div>\n"
 	#create the new directory heading, and then we will add all the video links in it
 	bodytext += ("\n<div>\n")
+	bodytext += ("&nbsp&nbsp&nbsp&nbsp&nbsp" * (level - 1))
 	if(level == 1):
-		bodytext += ("<h1 id=" + current_dir + ">" + current_dir + "</h1>")
+		bodytext += ('<span class="h1" id=' + current_dir + ">" + current_dir + "</span>")
 	elif(level == 2):
-		bodytext += ("<h2 id=" + current_dir + ">" + current_dir + "</h2>")
+		bodytext += ('<span class="h2" id=' + current_dir + ">" + current_dir + "</span>")
 	elif(level == 3):
-		bodytext += ("<h3 id=" + current_dir + ">" + current_dir + "</h3>")
+		bodytext += ('<span class="h3" id=' + current_dir + ">" + current_dir + "</span>")
 	elif(level >= 4):
-		bodytext += ("<h4 id=" + current_dir + ">" + current_dir + "</h4>")
+		bodytext += ('<span class="h4" id=' + current_dir + ">" + current_dir + "</span>")
 	bodytext += ("\n</div>\n")
+	#create the actual URLs
 	bodytext += "\n<div>\n"
 	for file in files: #for each file in the current directory
 		if file != ".DS_Store":
@@ -36,10 +41,15 @@ for root, dirs, files in os.walk(root_dir): #for each directory and subdirectory
 			url = f.readlines()[1][4:]
 			f.close()
 			#print out the URL as markdown
-			bodytext += ("- " + "<a href=" + url + " target=_blank>" + file[:-4] + "</a>\n")
-	bodytext += "\n</div>\n"
-output_file.write("Below are some videos I have liked on Youtube, organized into categories. I made this list by using Youtube Export, using a custom python script to get the link/text into Markdown format, and then manually moving the videos in the categories below.")
-output_file.write("---\nlayout: page\ntitle: Videos\n---\n")
-output_file.write("Index of videos: \n\n" + index + '\n')
+			bodytext += "<br>" + ("&nbsp&nbsp&nbsp&nbsp&nbsp" * (level - 1)) + ("- " + "<a href=" + url + " target=_blank>" + file[:-4] + "</a>\n")
+	bodytext += "\n</div><br>\n"
+
+output_file.write('<html> <head> <link rel="stylesheet" href="styles.css"> </head> <body>')
+output_file.write('<span class="vidintro">Below are some videos I have liked from across the Internet, organized into categories.')
+output_file.write(' Most come from Youtube, and were exported from my "Liked Videos" playlist using <a href= "https://sourceforge.net/projects/youtubeexport/">Youtube Export</a>.')
+output_file.write(' I manually moved all my video URLs into the categories below, and used a <a href="https://github.com/domcc3/url_organizer">script</a> to create this webpage. Please note that all links will open in a new window.')
+output_file.write('\n<br><br><div id="TOC"> Table of Contents</div></span><br>\n')
+output_file.write('<div class="index">' + index + '<br><br></div>')
 output_file.write(bodytext)
+output_file.write('</body></html>')
 output_file.close()
